093a67fe3469d2be41b2f1022bb8ec4e281c6c4b
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-09 00:47:03 +0100

    refactored base code

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,5 @@
 export { default as Header } from './Header';
 export { default as Card } from './Bid/Card';
-export { default as Maps } from './Patronise/Maps';
 export { default as BidNav } from "./Bid/BidNav"
 export { default as PatroniseNav } from "./Patronise/PatroniseNav"
 export { default as PatroniseCard } from "./Patronise/PatroniseCard"
