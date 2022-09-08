d3d92b41391d2065e3c4e861f0bea5f54dac9ae9
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-09 00:26:39 +0100

    updated assets folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
 import { Outlet, NavLink, useLocation } from "react-router-dom";
-import { IconContext } from "react-icons";
-import { TbWorld, TbLayoutGridAdd } from 'react-icons/tb'
-import { AiOutlineClockCircle, AiOutlinePlusCircle } from 'react-icons/ai'
-import pencil from "../assets/pencil.svg";
-import suitcase from "../assets/suitcase.svg";
+import { BidNav, PatroniseNav } from '../components';
 import styles from "../styles/Home.module.css";
 
 const Home = (): JSX.Element => {
+    let activeClassName = styles.side_active;
+    const { pathname } = useLocation();
 
-    const [nav, setNav] = useState(true);
+    let activeStyle = {
+        backgroundColor: 'var(--arkMarron)',
+        color: '#fff'
+    }
 
-    let activeClassName = styles.side_active
+    const nav = () => {
+        if (pathname === '/bid' || pathname === '/') {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
     return (
         <main className={styles.home}>
@@ -19,101 +26,22 @@ const Home = (): JSX.Element => {
             <section className={styles.side_nav}>
                 <ul>
                     <li>
-                        <NavLink className={({ isActive }) => isActive ? activeClassName : undefined} onClick={() => setNav(!nav)} to="Patronise">Patronise</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : undefined} to="patronise">Patronise</NavLink>
                     </li>
-                    <li>
-                        <NavLink className={({ isActive }) => isActive ? activeClassName : undefined} onClick={() => setNav(!nav)} to="bid">Bid</NavLink>
+                    <li >
+                        <NavLink className={({ isActive }) => isActive ? activeClassName : undefined} to="bid" style={nav() ? activeStyle : undefined}>Bid</NavLink>
                     </li>
                 </ul>
 
-                {
-                    nav &&
-                    <div className={styles.bid_nav}>
-                        <IconContext.Provider value={{ className: 'bid-icons' }}>
-                            <div>
-                                <h3>My Categories</h3>
-                                <div className={styles.bid_content}>
-                                    <p>Other- Admin Support</p>
-                                    <p>Personal/Virtual Assistant</p>
-                                    <p>Web Research</p>
-                                    <p>Pro</p>
-                                    <img src={pencil} alt="pencil" width="21" height="21" />
-                                </div>
-                            </div>
-                            <div>
-                                <div className={styles.pencil_heading}>
-                                    <h3>Visibility </h3>
-                                    <img className={styles.pencil} src={pencil} alt="pencil" width="19" height="19" />
-
-                                </div>
-
-                                <p className={styles.bid_sub}>
-                                    <span>
-                                        <TbWorld className={`${styles.bid_icon}`} />
-                                    </span>
-
-                                    <span>
-                                        Public
-                                    </span>
-                                </p>
-                            </div>
-                            <div>
-                                <div className={styles.pencil_heading}>
-                                    <h3>Availability </h3>
-                                    <img className={styles.pencil} src={pencil} alt="pencil" width="19" height="19" />
-
-                                </div>
-                                <p className={styles.bid_sub}>
-                                    <span>
-                                        <AiOutlineClockCircle className={`${styles.bid_icon}`} />
-                                    </span>
-                                    <span>
-                                        More than 30hr/week
-                                    </span>
-                                </p>
-                            </div>
-                            <div>
-                                <h3>Booth Completion</h3>
-                                <div className={styles.bid_completion}>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <p>100%</p>
-                                </div>
-                                <p className={styles.bid_sub}>
-                                    <span>
-                                        <AiOutlinePlusCircle className={`${styles.bid_icon}`} />
-                                    </span>
-                                    <span>
-                                        Add Work History
-                                    </span>
-                                </p>
-                            </div>
-                            <div>
-                                <h3>Inventory</h3>
-                                <div className={styles.pencil_heading}>
-                                    <img className={styles.suitcase} src={suitcase} alt="suitcase" width="" height="" />
-                                    <p>My inventory</p>
-                                </div>
-                            </div>
-                            <div>
-                                <h3>Bids</h3>
-                                <p className={styles.bid_sub}>
-                                    <span>
-                                        <TbLayoutGridAdd className={`${styles.bid_icon}`} />
-                                    </span>
-                                    <span>
-                                        50 available connects
-                                    </span>
-                                </p>
-                            </div>
-                        </IconContext.Provider>
-                    </div>
+                {nav() &&
+                    <BidNav />
 
                 }
 
-                {!nav && <div>
-                    no</div>}
+                {!nav() &&
+
+                    <PatroniseNav />
+                }
 
 
             </section>
